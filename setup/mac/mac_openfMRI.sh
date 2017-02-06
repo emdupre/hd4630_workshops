8a188a0cb0c18929f61d23abecf15bceeeab4986
emdupre <emd222@cornell.edu>
2017-02-06 02:52:48 -0500

    os specific setup scripts

---
+++ setup/mac/mac_openfMRI.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+curl -o ds102_R2.0.0.tgz http://openfmri.s3.amazonaws.com/tarballs/ds102_R2.0.0_all_data.tgz
+tar -xvzf ds102_R2.0.0.tgz
+rm ds102_R2.0.0.tgz
+
+curl -o ds000164_R1.0.0.zip https://s3.amazonaws.com/traverses3-openfmri/ds000164/ds000164_R1.0.0/compressed/ds000164_R1.0.0_sub001-028.zip
+unzip ds000164_R1.0.0.zip
+rm ds000164_R1.0.0.zip
+
+mkdir data
+mv ds102_R2.0.0 data/Flanker_task
+mv ds000164_R1.0.0 data/Stroop_task
