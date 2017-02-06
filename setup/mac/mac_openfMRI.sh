46c132866c5536ae994e8e2eadb6ffdf2166c661
Elizabeth DuPre <emd222@cornell.edu>
2017-02-06 09:58:35 -0500

    update data paths

--- setup/mac/mac_openfMRI.sh
+++ setup/mac/mac_openfMRI.sh
@@ -8,6 +8,6 @@ curl -o ds000164_R1.0.0.zip https://s3.amazonaws.com/traverses3-openfmri/ds00016
 unzip ds000164_R1.0.0.zip
 rm ds000164_R1.0.0.zip
 
-mkdir data
-mv ds102_R2.0.0 data/Flanker_task
-mv ds000164_R1.0.0 data/Stroop_task
+mkdir $HOME/Desktop/hd4630_workshops/data
+mv ds102_R2.0.0 $HOME/Desktop/hd4630_workshops/data/Flanker_task
+mv ds000164_R1.0.0 $HOME/Desktop/hd4630_workshops/data/Stroop_task
