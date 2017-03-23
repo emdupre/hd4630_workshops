7f08939a5d3551407825a956d64d9315cf037960
emdupre <emd222@cornell.edu>
2017-03-23 09:48:41 -0400

    Remove files now on master branch

--- setup/windows/windows_launch.sh
+++
@@ -1,4 +0,0 @@
-docker run -ti -e DISPLAY=$1:0 \
--v $HOME/Desktop/hd4630_workshops-master/data/:/home/student/data/ \
--v $HOME/Desktop/hd4630_workshops-master/code/:/home/student/code/ \
---rm emdupre/hd4630_workshops
